e117315572f1e05b90723e1c526980f4ec42b7d1
mnubhan <76425959+mnubhan@users.noreply.github.com>
2022-08-22 17:50:34 +0800

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,7 @@
+function hoopCount (n) {
+   return n>=10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+}
+
+function hoopCount (n) {
+  return (n < 10) ? 'Keep at it until you get it' : 'Great, now move on to tricks';
+}
